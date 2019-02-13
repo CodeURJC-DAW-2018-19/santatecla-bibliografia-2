deda78a26d8470ea7e4d4dab769269d455c17b0f
Sergio <kaoss176@gmail.com>
2019-02-13 22:26:05 +0100

    Copy log in
    It still does not work but is identical to the examples, we should ask the teacher

--- Bibliografia/src/main/java/dataBase/user/User.java
+++ Bibliografia/src/main/java/dataBase/user/User.java
@@ -15,6 +15,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+/**
+ * This is the entity to store in database user information. It contains the
+ * following basic information:
+ * <ul>
+ * <li>name: The name of the user. This name have to be used to logIn into the
+ * service</li>
+ * <li>passwordHash: The hash of the password. The password in never stored in
+ * plain text to avoid information leak</li>
+ * <li>roles: The roles of this user</li>
+ * 
+ * To check if a user can be logged into the service, this object is loaded from
+ * database and password is verified. If user is authenticated, then this
+ * database object is returned to the user.
+ * 
+ * NOTE: This class is intended to be extended by developer adding new
+ * attributes. Current attributes can not be removed because they are used in
+ * authentication procedures.
+ */
+
 @Entity
 public class User {
 
