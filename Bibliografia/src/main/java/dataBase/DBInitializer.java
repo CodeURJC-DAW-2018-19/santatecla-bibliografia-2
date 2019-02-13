f03352bcee7fc0369c4b69b204b49a67a29a5332
Natalia <natalia.madruenno@gmail.com>
2019-02-13 18:20:39 +0100

    Update autor

--- Bibliografia/src/main/java/dataBase/DBInitializer.java
+++ Bibliografia/src/main/java/dataBase/DBInitializer.java
@@ -4,7 +4,6 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import dataBase.user.User;
 import dataBase.user.UserRepository;
 
---
+++ Bibliografia/src/main/java/dataBase/autor/AutorRepository.java
@@ -0,0 +1,8 @@
+package dataBase.autor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+
+}
