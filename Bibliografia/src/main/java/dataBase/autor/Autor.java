a164a9a9e978cbbf7092d30f1e11c5d5cd628df4
Natalia <natalia.madruenno@gmail.com>
2019-02-13 17:55:39 +0100

    added  autor

--- Bibliografia/src/main/java/dataBase/autor/Autor.java
+++ Bibliografia/src/main/java/dataBase/autor/Autor.java
@@ -1,5 +1,17 @@
 package dataBase.autor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Autor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id = -1;
+	
+	private String nombre;
 
 }
