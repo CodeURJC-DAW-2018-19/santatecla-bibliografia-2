01fa2cc2c5d57372d66943cb0b8633adb275a026
Natalia <natalia.madruenno@gmail.com>
2019-02-16 17:54:34 +0100

    change initial tab

--- Bibliografia/src/main/java/dataBase/web/WebSecurityConfig.java
+++ Bibliografia/src/main/java/dataBase/web/WebSecurityConfig.java
@@ -18,28 +18,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
     	
     	// Public pages
-        http.authorizeRequests().antMatchers("/").permitAll();
-        http.authorizeRequests().antMatchers("/login").permitAll();
-        http.authorizeRequests().antMatchers("/loginerror").permitAll();
-        http.authorizeRequests().antMatchers("/logout").permitAll();
+    	 http.authorizeRequests().antMatchers("/").permitAll();
+    	 http.authorizeRequests().antMatchers("/login").permitAll();
+    	 http.authorizeRequests().antMatchers("/loginerror").permitAll();
+    	 http.authorizeRequests().antMatchers("/logout").permitAll();
+    	 // Private pages (all other pages)
+    	 http.authorizeRequests().anyRequest().authenticated();
+    	 // Login form
+    	 http.formLogin().loginPage("/login");
+    	 http.formLogin().usernameParameter("username");
+    	 http.formLogin().passwordParameter("password");
+    	 http.formLogin().defaultSuccessUrl("/home");
+    	 http.formLogin().failureUrl("/loginerror");
+    	 // Logout
+    	 http.logout().logoutUrl("/logout");
+    	 http.logout().logoutSuccessUrl("/");
 
-        // Private pages (all other pages)
-        http.authorizeRequests().antMatchers("/inicio").hasAnyRole("USER");
-        http.authorizeRequests().antMatchers("/newBook").hasAnyRole("ADMIN");
-        http.authorizeRequests().antMatchers("/editBook").hasAnyRole("ADMIN");
-        http.authorizeRequests().antMatchers("/editBook/**").hasAnyRole("ADMIN");
-        http.authorizeRequests().antMatchers("/deleteBook/**").hasAnyRole("ADMIN");
-
-        // Login form
-        http.formLogin().loginPage("/login");
-        http.formLogin().usernameParameter("username");
-        http.formLogin().passwordParameter("password");
-        http.formLogin().defaultSuccessUrl("/");
-        http.formLogin().failureUrl("/loginerror");
-
-        // Logout
-        http.logout().logoutUrl("/logout");
-        http.logout().logoutSuccessUrl("/");
+    	 // Disable CSRF at the moment
+    	 http.csrf().disable();
     }
 
     @Override
--- Bibliografia/src/main/java/tabs/Inicio.java
+++ Bibliografia/src/main/java/tabs/Inicio.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Inicio{
 
-	@RequestMapping("/inicio")
+	@RequestMapping("/")
 	public String greeting(Model model) {
 
 		return "inicio";
