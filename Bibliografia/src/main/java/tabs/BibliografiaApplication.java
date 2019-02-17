afd506562a76b777dedde3c1b978ef041bab682c
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-02-17 19:05:56 +0100

    Disable Spring Boot Security Auto-Configuration

--- Bibliografia/src/main/java/tabs/BibliografiaApplication.java
+++ Bibliografia/src/main/java/tabs/BibliografiaApplication.java
@@ -2,8 +2,9 @@ package tabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) 
 public class BibliografiaApplication {
 
 	public static void main(String[] args) {
