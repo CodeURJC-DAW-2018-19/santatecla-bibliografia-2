5f45aeaad9a7996c05d7ccd11592cd9be1a28f0a
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-02-18 00:21:33 +0100

    ObraRepository - findByTitle();

--- Bibliografia/src/main/java/dataBase/obra/ObraRepository.java
+++ Bibliografia/src/main/java/dataBase/obra/ObraRepository.java
@@ -1,7 +1,11 @@
 package dataBase.obra;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ObraRepository extends JpaRepository<Obra, Long> {
+	List<Obra> findByTitle (String title);
 	
 }
