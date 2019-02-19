60aad1d6f19dd6146961a8b9de481f4158a9ff06
ZhihaoDC <37367693+ZhihaoDC@users.noreply.github.com>
2019-02-19 19:46:41 +0100

    Add "Cita" sync with database

--- Ejemplo/src/main/java/es/daw/bibliografia/web/TemaController.java
+++ Ejemplo/src/main/java/es/daw/bibliografia/web/TemaController.java
@@ -1,14 +1,25 @@
 package es.daw.bibliografia.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import es.daw.bibliografia.book.CitaService;
+
+
 @Controller
 public class TemaController {
 	
+	@Autowired
+	private CitaService service;
+	
+	
 	@RequestMapping("/tema")
 	public String accederTema(Model model) {
+	
+		model.addAttribute("quotes", service.findAll());
 		return "tema";
 	}
 	
