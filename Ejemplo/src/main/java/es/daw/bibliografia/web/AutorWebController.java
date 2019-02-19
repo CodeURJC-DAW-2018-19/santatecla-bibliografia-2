ea50d887c7e6edbe711fa6dcd143eae6ca22f897
Natalia <natalia.madruenno@gmail.com>
2019-02-19 18:14:18 +0100

    init loop data bases

--- Ejemplo/src/main/java/es/daw/bibliografia/web/AutorWebController.java
+++ Ejemplo/src/main/java/es/daw/bibliografia/web/AutorWebController.java
@@ -1,14 +1,22 @@
 package es.daw.bibliografia.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import es.daw.bibliografia.book.BookService;
+import es.daw.bibliografia.book.ObraService;
+
 @Controller
 public class AutorWebController {
 	
+	@Autowired
+	private ObraService obraService;
+	
 	@RequestMapping("/autor")
 	public String showAutor(Model model) {
+		model.addAttribute("obras", obraService.findAll());
 		return "autor";
 	}
 	
