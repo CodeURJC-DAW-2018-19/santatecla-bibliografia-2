bb4c065d36fc17bf31238ad324eea02e8b0c69f8
Natalia <natalia.madruenno@gmail.com>
2019-02-19 18:24:32 +0100

    Done table autor tab

--- Ejemplo/src/main/java/es/daw/bibliografia/web/AutorWebController.java
+++ Ejemplo/src/main/java/es/daw/bibliografia/web/AutorWebController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.daw.bibliografia.book.BookService;
+import es.daw.bibliografia.book.CitaService;
 import es.daw.bibliografia.book.ObraService;
+import es.daw.bibliografia.book.TemaService;
 
 @Controller
 public class AutorWebController {
@@ -14,9 +16,17 @@ public class AutorWebController {
 	@Autowired
 	private ObraService obraService;
 	
+	@Autowired
+	private TemaService temaService;
+	
+	@Autowired
+	private CitaService citaService;
+	
 	@RequestMapping("/autor")
 	public String showAutor(Model model) {
 		model.addAttribute("obras", obraService.findAll());
+		model.addAttribute("temas", temaService.findAll());
+		model.addAttribute("citas", citaService.findAll());
 		return "autor";
 	}
 	
