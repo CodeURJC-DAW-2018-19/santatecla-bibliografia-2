9279d6eabf93c09ccafc4f96c47f7db50e6d02c2
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-02-20 23:21:51 +0100

    Created obraShow & obraShowError

--- Ejemplo/src/main/java/es/daw/bibliografia/web/ObraController.java
+++ Ejemplo/src/main/java/es/daw/bibliografia/web/ObraController.java
@@ -1,12 +1,16 @@
 package es.daw.bibliografia.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.daw.bibliografia.book.AutorService;
+import es.daw.bibliografia.book.CitaService;
 import es.daw.bibliografia.book.Obra;
 import es.daw.bibliografia.book.ObraService;
 import es.daw.bibliografia.book.TemaService;
@@ -22,6 +26,9 @@ public class ObraController {
 
 	@Autowired
 	private AutorService serviceAutor;
+	
+	@Autowired
+	private CitaService serviceCita;
 	
 	@Autowired
 	private BookWebController webController;
@@ -42,12 +49,25 @@ public class ObraController {
 		
 		return "obra";
 	}
-	@RequestMapping ("/obra/{id}")
+	@GetMapping ("/obra/{id}")
 	public String abrirObra(Model model, @PathVariable long id) {
 		
-		//Obra obra= repository.findById(id);
+		Optional<Obra> obra= service.findOne(id);
 		
+		model.addAttribute("autores", serviceAutor.findAll());
+		model.addAttribute("temas", serviceTema.findAll());
+		model.addAttribute("citas", serviceCita.findAll());
 		
-		return "obra"; 
+		if(obra.isPresent()) {
+			
+			model.addAttribute("title", obra.get().getTitle());
+			model.addAttribute("URL", obra.get().getURL());
+			model.addAttribute("date", obra.get().getDate());
+			model.addAttribute("editorial", obra.get().getEditorial());
+			model.addAttribute("url_editorial", obra.get().getUrl_editorial());
+			return "obraShow"; 
+		}else {
+			return "obraShowError"; 
+		}		
 	}
 }
