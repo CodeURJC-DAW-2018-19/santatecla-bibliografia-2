d25d22fbdeee2affeda1d1f87068d8228c8358fa
ZhihaoDC <37367693+ZhihaoDC@users.noreply.github.com>
2019-02-20 18:43:30 +0100

    Sync tema with DB & Fix errors in footer
    Tema now loads data form DataBase.
    Footer now displays "ñ" correctly.

--- Ejemplo/src/main/java/es/daw/bibliografia/web/TemaController.java
+++ Ejemplo/src/main/java/es/daw/bibliografia/web/TemaController.java
@@ -6,18 +6,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import es.daw.bibliografia.book.AutorService;
 import es.daw.bibliografia.book.CitaService;
+import es.daw.bibliografia.book.ObraService;
 
 @Controller
 public class TemaController {
 
 	@Autowired
-	private CitaService service;
-
+	private CitaService quotes;
+	
+	@Autowired
+	private ObraService works;
+	
+	@Autowired
+	private AutorService authors;
+	
 	@RequestMapping("/tema")
 	public String accederTema(Model model) {
-
-		model.addAttribute("quotes", service.findAll());
+		model.addAttribute("quotes", quotes.findAll());
+		model.addAttribute("works", works.findAll());
+		model.addAttribute("authors", authors.findAll());
 		return "tema";
 	}
 
