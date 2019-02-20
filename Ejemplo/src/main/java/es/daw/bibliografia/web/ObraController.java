6c25d14bb4365075725767c62fdcc76e2ca63cec
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-02-20 18:54:17 +0100

    Save Literary Work in BBDD

--- Ejemplo/src/main/java/es/daw/bibliografia/web/ObraController.java
+++ Ejemplo/src/main/java/es/daw/bibliografia/web/ObraController.java
@@ -6,26 +6,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import es.daw.bibliografia.book.Cita;
 import es.daw.bibliografia.book.Obra;
 import es.daw.bibliografia.book.ObraService;
 
 @Controller
 public class ObraController {
-	
+
 	@Autowired
 	private ObraService service;
 	
 	@RequestMapping("/obra/guardada") //Esto va en BookWebController Cuando se pulsa el boton de new
-	public String añadirAutor(Model model, Obra obra, Cita cita) {
-		
+	public String añadirObra(Model model, Obra obra) {
 		service.save(obra);
 		return "Index"; 
 	}
 	
 	@RequestMapping("/obra/new")
 	public String irObra(Model model) {
-
+		
 		return "obra";
 	}
 	@RequestMapping ("/obra/{id}")
