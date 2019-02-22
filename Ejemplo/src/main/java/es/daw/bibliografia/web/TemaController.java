6c0f045ff80e6aae6b194b991ded4df7e41211ab
ZhihaoDC <37367693+ZhihaoDC@users.noreply.github.com>
2019-02-22 19:34:14 +0100

    Update TemaController
    Added "Referencias" section Sync with DataBase

--- Ejemplo/src/main/java/es/daw/bibliografia/web/TemaController.java
+++ Ejemplo/src/main/java/es/daw/bibliografia/web/TemaController.java
@@ -1,28 +1,43 @@
 package es.daw.bibliografia.web;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.daw.bibliografia.book.AutorService;
+import es.daw.bibliografia.book.Cita;
 import es.daw.bibliografia.book.CitaService;
 import es.daw.bibliografia.book.ObraService;
+import es.daw.bibliografia.book.Tema;
+import es.daw.bibliografia.book.TemaService;
+import es.daw.bibliografia.book.Obra;
+import es.daw.bibliografia.book.Autor;
 import es.daw.bibliografia.user.Tabs;
 import es.daw.bibliografia.user.UserComponent;
 
 @Controller
 public class TemaController {
+	
+	@Autowired
+	private TemaService temaService;
 
 	@Autowired
-	private CitaService quotes;
+	private CitaService citaService;
 
 	@Autowired
-	private ObraService works;
+	private ObraService obraService;
 
 	@Autowired
-	private AutorService authors;
+	private AutorService autorService;
 
 	@Autowired
 	private BookWebController webController;
@@ -30,15 +45,31 @@ public class TemaController {
 	@Autowired
 	private UserComponent userComponent;
 
-	@RequestMapping("/tema")
-	public String accederTema(Model model) {
-		model.addAttribute("quotes", quotes.findAll());
-		model.addAttribute("works", works.findAll());
-		model.addAttribute("authors", authors.findAll());
-
+	@RequestMapping("/tema/{id}")
+	public String accederTema(Model model, @PathVariable long id) {
+		Optional <Tema> tema = temaService.findOne(id);
 		webController.addUserToModel(model);
-
-		return "tema";
+		
+		if(tema.isPresent()) {
+			Tema theme= tema.get();
+			List<Obra> obras = theme.getObras();
+			List<Cita> citas = new ArrayList<>();
+			List<Autor> autores = new ArrayList<>();
+			for(int i=0; i<obras.size(); i++) {
+				citas = Stream.concat(citas.stream(), obras.get(i).getCitas().stream())
+                        .collect(Collectors.toList());
+				autores = Stream.concat(autores.stream(), obras.get(i).getAutores().stream())
+                        .collect(Collectors.toList());
+			}
+			
+			model.addAttribute("obras", obras);
+			model.addAttribute("citas", citas);
+			model.addAttribute("autores", autores);
+			
+			return "tema";
+		}
+		else
+			return "temaError";
 	}
 
 	private void userTabs(Model model, String url, String name, boolean active) {
