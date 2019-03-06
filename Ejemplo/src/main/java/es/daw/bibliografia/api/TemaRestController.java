435f1fe90afe1c1e483d27e986da72497c952e5d
ZhihaoDC <37367693+ZhihaoDC@users.noreply.github.com>
2019-03-06 19:46:12 +0100

    Create REST Controller package & Initialize REST Security Config

---
+++ Ejemplo/src/main/java/es/daw/bibliografia/api/TemaRestController.java
@@ -0,0 +1,63 @@
+package es.daw.bibliografia.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.daw.bibliografia.book.Cita;
+import es.daw.bibliografia.book.CitaService;
+import es.daw.bibliografia.book.Tema;
+import es.daw.bibliografia.book.TemaService;
+
+
+@RestController
+public class TemaRestController {
+	
+	@Autowired
+	private TemaService temaService;
+	
+	@Autowired
+	private CitaService citaService;
+	
+	
+	
+	@DeleteMapping("/api/tema/borrarCita/{id}")
+	public Cita deleteCita(@PathVariable long id) {
+
+		Cita deletedCita= citaService.findOne(id).get();
+		citaService.delete(id);
+
+		return deletedCita;
+	}
+
+	@DeleteMapping("/tema/borrar")
+	public Tema deleteTema(@RequestParam String contenido) {
+		
+		Tema deletedTema= temaService.findOneByContenido(contenido).get();
+		temaService.delete(temaService.findOneByContenido(contenido).get().getId());
+		
+		return deletedTema;
+	}
+
+	@PostMapping("/tema/guardado")
+	public String addAutor(Tema tema) {
+		temaService.save(tema);
+
+
+		return "redirect:/temashow/".concat(tema.getContenido());
+	}
+
+	@DeleteMapping("/tema/{nombreTema}/borrar/autor")
+	public String deleteTema2(@PathVariable("nombreTema") String nombreObra,
+			@RequestParam("nombreAutor") String autor) {
+		temaService.delete(temaService.findOneByContenido(nombreObra).get().getId());
+
+
+		return "redirect:/autorshow/".concat(autor);
+	}
+	
+	
+}
---
+++ Ejemplo/src/main/java/es/daw/bibliografia/security/RestSecurityConfig.java
@@ -0,0 +1,50 @@
+package es.daw.bibliografia.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(1)
+public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public UserRepositoryAuthProvider userRepoAuthProvider;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.antMatcher("/api/**");
+		
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/logIn").authenticated();
+		
+		// URLs that need authentication to access to it
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/tema/").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/tema/**").hasRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/tema/**").hasRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/tema/**").hasRole("ADMIN");		
+		
+		// Other URLs can be accessed without authentication
+		http.authorizeRequests().anyRequest().permitAll();
+
+		// Disable CSRF protection (it is difficult to implement in REST APIs)
+		http.csrf().disable();
+
+		// Use Http Basic Authentication
+		http.httpBasic();
+
+		// Do not redirect when logout
+		http.logout().logoutSuccessHandler((rq, rs, a) -> {	});
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// Database authentication provider
+		auth.authenticationProvider(userRepoAuthProvider);
+	}
+}
