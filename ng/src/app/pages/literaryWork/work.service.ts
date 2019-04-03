fe81b83848235092c5573d6f61df4546dac857a2
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-04-03 21:51:38 +0200

    Literary Work service

--- ng/src/app/pages/literaryWork/work.service.ts
+++ ng/src/app/pages/literaryWork/work.service.ts
@@ -1,9 +1,131 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs'; 
+import { catchError } from 'rxjs/operators';
+import { LoginService } from '../../auth/login.service';
 
+
+export interface Work {
+  id?: number; 
+  title: string; 
+  URL: string; 
+  date: string; 
+  editorial: string; 
+  url_editorial: string;
+}
+
+export interface Quote {
+  contenido : string; 
+}
+
+export interface Author {
+  id?: number;
+  nombre: string;
+  url_foto: string;
+  fecha_nac: string;
+  fecha_def: string;
+  url_mapa: string;
+  lugar: string;
+}
+export interface Theme {
+  id?: number;
+  obras: Array<Work>;
+  contenido: string;
+  numObras: number;
+}
+
+const URL='/api/obras/';
 @Injectable({
   providedIn: 'root'
 })
 export class WorkService {
 
-  constructor() { }
+  constructor(private loginService: LoginService, private http: HttpClient) { }
+  
+  private handleError(error: any) {
+    console.error(error);
+    return Observable.throw('Server error (' + error.status + '): ' + error);
+  }
+// LITERARY WORK: 
+  getWorks(): Observable<Work[]> {
+    return this.http.get<Work[]>(URL, { withCredentials: true }).pipe(catchError((error) => this.handleError(error)));
+  }
+  
+  getWork(title: string): Observable<Work> {
+    return this.http.get<Work>(URL + title, { withCredentials: true }).pipe(catchError((error) => this.handleError(error)));
+  }
+
+
+  saveWork(work: Work, author: Author, theme: Theme): Observable<Work> {
+ //AddObra & EditObra
+    const body = JSON.stringify(work);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    if (!work.id) {
+      return this.http
+          .post<Work>(URL + author.id + theme.id, body, { headers })
+          .pipe(catchError((error) => this.handleError(error)));
+    } else {
+      return this.http
+          .put<Work>(URL, body, { headers })
+          .pipe(catchError((error) => this.handleError(error)));
+    }
+  }
+ 
+  
+  editWorkLinkAuthor(work: Work, author: Author): Observable<Work> {
+    const body = JSON.stringify(work);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    return this.http
+          .put<Work>('/api/obras/autor/'+work.id + author.id, body, { headers })
+          .pipe(catchError((error) => this.handleError(error)));
+
+  }
+
+  removeWork(work: Work): Observable<Work> {
+    return this.http
+        .delete<Work>(URL + work.title)
+        .pipe(catchError((error) => this.handleError(error)));
+  }
+
+  removeWorkInAuthor(work: Work): Observable<Work> {
+    return this.http
+        .delete<Work>('/api/obra/' + work.title +'/borrar/autor')
+        .pipe(catchError((error) => this.handleError(error)));
+  }
+
+  removeWorkInTheme(work: Work): Observable<Work> {
+    return this.http
+        .delete<Work>('/api/obra/' + work.title +'/borrar/tema')
+        .pipe(catchError((error) => this.handleError(error)));
+  }
+
+  // addQuote in Literary Work 
+  addQuote(quote: Quote, work: Work): Observable<Quote> {
+    const body = JSON.stringify(work);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    if (!work.id) {
+      return this.http
+          .post<Quote>('/api/obras/cita/'+work.id, body, { headers })
+          .pipe(catchError((error) => this.handleError(error)));
+    } 
+  }
+
+
+
+
+
+
+
+
 }
