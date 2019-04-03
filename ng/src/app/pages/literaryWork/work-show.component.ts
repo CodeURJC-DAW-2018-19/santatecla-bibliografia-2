b02803c42ac63b4dc62d02ff1b685da80444cc05
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-04-03 23:57:14 +0200

    Create Work-show.component

--- ng/src/app/pages/literaryWork/work-show.component.ts
+++ ng/src/app/pages/literaryWork/work-show.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Work, WorkService } from './work.service';
+import { LoginService } from '../../auth/login.service';
+import { TdDialogService } from '@covalent/core';
 
 @Component({
-  selector: 'app-work-show',
-  templateUrl: './workShow.component.html'
+  selector: 'work-show-component',
+  templateUrl: './work-show.component.html'
 })
-export class WorkShowComponent implements OnInit {
+export class WorkShowComponent {
+  work: Work;
 
-  constructor() { }
+  constructor(private router: Router, activatedRoute: ActivatedRoute, public service: WorkService, public loginService: LoginService, private _dialogService: TdDialogService) {
+    const id = activatedRoute.snapshot.params['id'];
+    service.getWork(id).subscribe((work) => (this.work = work), (error) => console.error(error));
+  }
 
-  ngOnInit() {
+  
+  removeWork() {
+    this._dialogService.openConfirm({
+      message: 'Do you want to remove this Work?',
+      title: 'Confirm',
+      width: '500px',
+      height: '175px'
+    }).afterClosed().subscribe((accept: boolean) => {
+      if (accept) {
+        this.service
+            .removeWork(this.work)
+            .subscribe((_) => this.router.navigate(['/']), (error) => console.error(error));
+      }
+    });
+  }
+  updateWork() {
+    this.router.navigate(['/work/update', this.work.id]);
   }
 
 }
