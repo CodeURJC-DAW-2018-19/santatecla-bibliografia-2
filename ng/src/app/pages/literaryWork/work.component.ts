bd5f47d87da28ff3da3b59d3dbb6b561847b82c2
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-04-03 23:58:05 +0200

    Create work.component.ts

--- ng/src/app/pages/literaryWork/work.component.ts
+++ ng/src/app/pages/literaryWork/work.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Work, WorkService } from './work.service';
+import { LoginService } from '../../auth/login.service';
+import { TdDialogService } from '@covalent/core';
 
 @Component({
-  selector: 'app-work',
+  selector: 'work-component',
   templateUrl: './work.component.html'
 })
-export class WorkComponent implements OnInit {
+export class WorkComponent {
+  work: Work;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router, activatedRoute: ActivatedRoute, public service: WorkService, public loginService: LoginService, private _dialogService: TdDialogService) {
+    const id = activatedRoute.snapshot.params['id'];
+    service.getWork(id).subscribe((work) => (this.work = work), (error) => console.error(error));
   }
+  
+
+
+ 
 
 }
