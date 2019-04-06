e6a419118bdb2e9c21db6800ce66fac30f0d7246
Israel Peñalver Sánchez <israelsonseca@gmail.com>
2019-04-06 17:18:25 +0200

    ThemesList

--- ng/src/app/app.routing.ts
+++ ng/src/app/app.routing.ts
@@ -4,12 +4,23 @@ import { BookListComponent } from './book-list.component';
 import { BookDetailComponent } from './book-detail.component';
 import { BookFormComponent } from './book-form.component';
 
+//ours
+import { ThemeListComponent } from './pages/theme/theme-list.component';
+import { ThemeDetailComponent } from './pages/theme/theme-detail.component';
+import { ThemeFormComponent } from './pages/theme/theme-form.component';
+
 const appRoutes = [
-  { path: 'books', component: BookListComponent, useAsDefault: true },
+/*   { path: 'books', component: BookListComponent, useAsDefault: true },
   { path: 'book/new', component: BookFormComponent },
   { path: 'book/:id', component: BookDetailComponent },
   { path: 'book/edit/:id', component: BookFormComponent },
-  { path: '', redirectTo: 'books', pathMatch: 'full' }
+  { path: '', redirectTo: 'books', pathMatch: 'full' }, */
+
+  { path: 'themes', component: ThemeListComponent, useAsDefault: true },
+  { path: 'theme/new', component: ThemeFormComponent },
+  { path: 'theme/:id', component: ThemeDetailComponent },
+  { path: 'theme/edit/:id', component: ThemeFormComponent },
+  { path: '', redirectTo: 'themes', pathMatch: 'full' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
