cafd1363c26677596990d51d3f5749c2db077329
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-04-06 21:53:34 +0200

    Update work-form.component html & ts

--- ng/src/app/pages/literaryWork/work-form.component.ts
+++ ng/src/app/pages/literaryWork/work-form.component.ts
@@ -1,23 +1,37 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Work, WorkService } from './work.service';
+import { LoginService } from '../../auth/login.service';
+import { TdDialogService } from '@covalent/core';
+import { Author } from '../author/author.service';
+import { Theme } from '../theme/theme.service';
 
 //Create new work
 @Component({
+    selector: 'work-formC',
     templateUrl: 'work-form.component.html',
 })
 export class WorkFormComponent {
     
     newWork: boolean;
     work: Work;
+    author: Author;
+    theme: Theme;
 
-    constructor(private _router: Router, activatedRoute: ActivatedRoute, private service: WorkService) {
+    constructor(private _router: Router, activatedRoute: ActivatedRoute, private service: WorkService, public loginService: LoginService, private _dialogService: TdDialogService) {
+        /*const title = activatedRoute.snapshot.params['title'];
+        if (title) {
+            service.getWork(title).subscribe((work) => (this.work = work), (error) => console.error(error));
+            //HERE ADD THIS.ATHOR & THIS.THEME BUT IDK
+            this.newWork = false;*/
         const id = activatedRoute.snapshot.params['id'];
         if (id) {
             service.getWork(id).subscribe((work) => (this.work = work), (error) => console.error(error));
+            //HERE ADD THIS.ATHOR & THIS.THEME BUT IDK
             this.newWork = false;
         } else {
             this.work = {  title: '', URL: '', date: '', editorial: '', url_editorial: ''};
+            //HERE ADD THIS.ATHOR & THIS.THEME BUT IDK
             this.newWork = true;
         }
     }
@@ -26,11 +40,12 @@ export class WorkFormComponent {
         window.history.back();
     }
 
-/*     save() {
-        this.service.saveWork(this.work, this.author, this.theme).subscribe(
+
+    save() {
+        this.service.saveWork(this.work,this.author,this.theme).subscribe(
             _ => {},
             (error: Error) => console.error('Error creating new work: ' + error),
         );
         window.history.back();
-    } */
+      }
 }
