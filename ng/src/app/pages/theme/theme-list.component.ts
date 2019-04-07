1632f41cbcb7979f59e908f53fd69950c0b5b459
Israel Peñalver Sánchez <israelsonseca@gmail.com>
2019-04-08 01:17:55 +0200

    removingThemesFrom Index

--- ng/src/app/pages/theme/theme-list.component.ts
+++ ng/src/app/pages/theme/theme-list.component.ts
@@ -3,15 +3,20 @@ import { Router } from '@angular/router';
 
 import { Theme, ThemeService } from './theme.service';
 import { LoginService } from '../../auth/login.service';
+import { TdDialogService } from '@covalent/core';
+
 
 @Component({
   selector: 'themes',
   templateUrl: 'theme-list.component.html',
   styleUrls: ['./theme-list.component.css']
+
 })
 export class ThemeListComponent implements OnInit {
 
   themes: Theme[];
+  private _dialogService: TdDialogService;
+
 
   constructor(private router: Router, private service: ThemeService,
     public loginService: LoginService) { }
@@ -28,6 +33,18 @@ export class ThemeListComponent implements OnInit {
   }
 
   deleteTheme(theme:Theme){
-    this.service.removeTheme(theme)
-  }
+
+    this._dialogService.openConfirm({
+        message: 'Do you want to remove this theme?',
+        title: 'Confirm', 
+        width: '500px', 
+        height: '175px'
+    }).afterClosed().subscribe((accept: boolean) => {
+        if (accept) {
+            this.service
+                .removeTheme(theme)
+                .subscribe((_) => this.router.navigate(['']), (error) => console.error(error));
+        }
+    });
+}
 }
