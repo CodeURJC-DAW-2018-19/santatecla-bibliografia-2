76bfd1a3bcf4575ef4f39feb5b5fc0ccbc17394c
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-04-08 04:19:12 +0200

    Work delete button || index

--- ng/src/app/pages/literaryWork/work-list.component.ts
+++ ng/src/app/pages/literaryWork/work-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { TdDialogService } from '@covalent/core';
 import { Work, WorkService } from './work.service';
 import { LoginService } from '../../auth/login.service';
 
@@ -14,7 +14,7 @@ export class WorkListComponent implements OnInit {
 
   works: Work[];
 
-  constructor(private router: Router, private service: WorkService,
+  constructor(private router: Router, private service: WorkService,private _dialogService: TdDialogService,
     public loginService: LoginService) { }
 
   ngOnInit() {
@@ -26,5 +26,18 @@ export class WorkListComponent implements OnInit {
 
   newWork() {
     this.router.navigate(['/work/new']);
+  }
+  deleteWork(work: Work){
+    this._dialogService.openConfirm({
+            message: 'Do you want to remove this work?',
+            title: 'Confirm', 
+            width: '500px', 
+            height: '175px'
+        }).afterClosed().subscribe((accept: boolean) => {
+            if (accept) {
+              this.router.navigate(['/work/'+work.title]);
+    this.service.removeWork(work).subscribe(() => this.router.navigate(['/']), (error) => console.error(error));
+    }
+        });
   }
 }
