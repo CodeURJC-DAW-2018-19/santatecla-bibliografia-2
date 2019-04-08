8d4947b393e62355ae308889abf242930d74073f
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-04-09 01:55:22 +0200

    Delete unnecesary class

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -88,11 +88,9 @@ import {
 
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DomSanitizer } from '@angular/platform-browser';
-import { BookService } from './book.service';
+
 import { LoginService } from './auth/login.service';
-import { BookDetailComponent } from './book-detail.component';
-import { BookListComponent } from './book-list.component';
-import { BookFormComponent } from './book-form.component';
+
 import { LoginComponent } from './login.component';
 import { routing } from './app.routing';
 import { ErrorInterceptor } from './auth/error.interceptor';
@@ -186,7 +184,7 @@ import { ThemeService } from './pages/theme/theme.service'; */
         routing,
     ],
     declarations: [AppComponent,
-         BookDetailComponent, BookListComponent, BookFormComponent, LoginComponent,
+          LoginComponent,
           ThemeListComponent, ThemeDetailComponent, ThemeFormComponent,
            AuthorDetailComponent, AuthorListComponent, AuthorFormComponent,
             WorkDetailComponent , WorkListComponent, WorkFormComponent,IndexComponent,
@@ -196,7 +194,7 @@ import { ThemeService } from './pages/theme/theme.service'; */
     bootstrap: [AppComponent],
 
 
-    providers: [BookService, LoginService, ThemeService, AuthorService, WorkService,
+    providers: [ LoginService, ThemeService, AuthorService, WorkService,
         { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
         { provide: LocationStrategy, useClass: HashLocationStrategy }
@@ -252,4 +250,4 @@ export class AppModule {
 constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
     matIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('/assets/symbol-defs.svg'));
 }
-}
+} 
