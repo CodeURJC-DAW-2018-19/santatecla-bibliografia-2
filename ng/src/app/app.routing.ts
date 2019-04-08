cd3d720fddf7778c30532e85083ae448d8ca3dfc
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-04-08 03:27:40 +0200

    Update app.routing.ts

--- ng/src/app/app.routing.ts
+++ ng/src/app/app.routing.ts
@@ -8,10 +8,13 @@ import { BookFormComponent } from './book-form.component';
 import { IndexComponent } from './index.component';
 import { ThemeDetailComponent } from './pages/theme/theme-detail.component';
 import { ThemeFormComponent } from './pages/theme/theme-form.component';
+import { ThemeListComponent } from './pages/theme/theme-list.component';
 import { WorkFormComponent } from './pages/literaryWork/work-form.component';
 import { WorkDetailComponent } from './pages/literaryWork/work-detail.component';
+import { WorkListComponent } from './pages/literaryWork/work-list.component';
 import { AuthorFormComponent } from './pages/author/author-form.component';
 import { AuthorDetailComponent } from './pages/author/author-detail.component';
+import { AuthorListComponent } from './pages/author/author-list.component';
 
 
 const appRoutes = [
@@ -25,12 +28,15 @@ const appRoutes = [
   { path: 'theme/new', component: ThemeFormComponent },
   { path: 'theme/:content', component: ThemeDetailComponent },
   { path: 'theme/edit/:content', component: ThemeFormComponent },
+  { path: 'themes', component: ThemeListComponent },
   { path: 'work/new', component: WorkFormComponent },
   { path: 'work/:title', component: WorkDetailComponent },
   { path: 'work/edit/:title', component: WorkFormComponent },
+  { path: 'works', component: WorkListComponent },
   { path: 'author/new', component: AuthorFormComponent },
   { path: 'author/:name', component: AuthorDetailComponent },
   { path: 'author/edit/:nombre', component: AuthorFormComponent },
+  { path: 'authors', component: AuthorListComponent },
   { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
