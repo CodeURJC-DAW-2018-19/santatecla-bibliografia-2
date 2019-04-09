5903947c65ed53ba04ef61ac7d0e04bcc04a6073
ZhihaoDC <37367693+ZhihaoDC@users.noreply.github.com>
2019-04-09 04:38:08 +0200

    Fix URL style from Rest API (Download PDF)

--- Ejemplo/src/main/java/es/daw/bibliografia/api/PDFRestController.java
+++ Ejemplo/src/main/java/es/daw/bibliografia/api/PDFRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PDFRestController {
 	
 	
-	@RequestMapping("/api/crearPDF/{contenido}")
+	@RequestMapping("/api/temas/{contenido}/PDF")
 	public void handleFileDownloadPDF(HttpServletResponse res, @PathVariable String contenido)
 			throws FileNotFoundException, IOException {
 
